92c0b15c68d2d76ddff747de551000c5a788f865
Tushar Mahajan <tusharmahajan080@gmail.com>
2024-09-29 14:43:38 +0530

    Env file edited

--- Config/db.js
+++ Config/db.js
@@ -2,13 +2,10 @@ const mongoose = require('mongoose');
 require('dotenv').config(); // Load environment variables
 
 const connectDB = () => {
+    console.log('MongoDB URI:', process.env.MONGODB_URI); // Add this log to check the value
     mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 };
 
 module.exports = connectDB;
-
-//https://napixbackend-2.onrender.com/api/users/signup
-
-
